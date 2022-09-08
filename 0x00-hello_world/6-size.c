f3abdebd9c81a095a6c7a59c0a4b74dc72a1b96a
Benneth <enwerembenneth@gmail.com>
2022-09-08 10:10:52 -0700

    Task6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <studio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */int main(void)
+{
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
+
+	printf("Size of a char: %1u byte(s)\n" , (unsigned long)sizeof(d));
+	printf("Size of a an int: %1u byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %1u byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %1u byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %1u byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
