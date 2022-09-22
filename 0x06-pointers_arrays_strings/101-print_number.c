b1c6ad5c6b8ee921cfed93bb65c5b0411f7d1902
Benneth <110991703+Benclassic@users.noreply.github.com>
2022-09-22 08:32:56 +0100

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,4 +1,4 @@
-#include "main.h"
+##include "main.h"
 
 /**
   * print_number - print numbers chars
@@ -22,5 +22,4 @@ void print_number(int n)
 	{
 		print_number(n1 / 10);
 	}
-	_putchar((n1 % 10) + '0');
-}
+	_putchar((n1 % 10) + '0'
