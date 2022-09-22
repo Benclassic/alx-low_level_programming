e732e185964a08bec4a209c6d90024e36887468a
Benneth <110991703+Benclassic@users.noreply.github.com>
2022-09-22 07:26:36 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,20 +1,18 @@
 #include "main.h"
 
 /**
- * _strcmp - Compare two strings
- * @s1: string
- * @s2: string
- * Return: negative int if s1 < s2, 0 if matching, and positive int if s1 > s2
+ * _strcmp - function to copy string
+ *
+ * @s1: first string to compare
+ * @s2: second string to compare
+ * Return: int
  */
-
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	while (*s1 && *s1 == *s2)
 	{
-		if (s1[i] !=s2[i])
-			return (s1[i] - s2[1]);
+		++s1;
+		++s2;
 	}
-	return (0);
+	return ((int)(unsigned char)(*s1) - (int)(unsigned char)(*s2));
 }
