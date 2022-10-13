92554a31a4d1aad18a689ece9f573266b4fd42e0
Benneth <enwerembenneth@gmail.com>
2022-10-12 23:40:11 -0700

    Task 100

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,15 @@
+nclude <stdio.h>
+
+#include <stdlib.h>
+
+/**
+ *
+ *  * print_opcodes - program that prints the opcodes of its own main function.
+ *
+ *   * @a: address of the main function
+ *
+ *    * @n: number of bytes to print
+ *
+ *     * Return: nothing.
+ *
+ *      */
