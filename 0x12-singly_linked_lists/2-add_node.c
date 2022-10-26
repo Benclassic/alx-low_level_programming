2134a7372e4a84a156cd88fb362eff4f513f9fb7
Benneth <110991703+Benclassic@users.noreply.github.com>
2022-10-26 10:03:33 +0100

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,15 +1,29 @@
-nclude "lists.h"
-
-
-
+#include "lists.h"
 /**
- *
- *  * listint_len - Entry point.
- *
- *   * @h:value.
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * add_node - Entry point.
+ * @head: pointer to the head.
+ * @str: value
+ * Return: Always 0 (Success)
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+	int a = 0;
 
-size_t listint_l
+	new_node = (list_t *)malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	while (*(str + a) != '\0')
+	{
+		a++;
+	}
+
+	new_node->len = a;
+	new_node->str = strdup(str);
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
