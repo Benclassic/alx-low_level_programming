a05d0a5dda1c1ed8bd5e53574da71012a14e8a52
Benneth <110991703+Benclassic@users.noreply.github.com>
2022-10-26 09:27:20 +0100

    Update 3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,11 +1,36 @@
-nclude "lists.h"
-
-
+#include "lists.h"
 
 /**
- *
- *  * print_listint - Entry point.
- *
- *   * @h:value.
- *
- *    * Return: Always 0 (
+ * add_nodeint_end - Entry point.
+ * @head:value pointer to pointer.
+ * @n: value
+ * Return: Always 0 (Success)
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *nend, *node;
+
+	nend = malloc(sizeof(listint_t));
+	if (nend == NULL)
+	{
+		return (NULL);
+	}
+
+	nend->n  = n;
+	nend->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = nend;
+	}
+	else
+	{
+		node = *head;
+		while (node->next != NULL)
+		{
+			node = node->next;
+		}
+		node->next = nend;
+	}
+	return (nend);
+}
