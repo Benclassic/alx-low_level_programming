08791d608e218f22d6e8f907dac9197e9767a782
Benneth <110991703+Benclassic@users.noreply.github.com>
2022-10-26 09:28:50 +0100

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,11 +1,26 @@
-nclude "lists.h"
-
-
+#include "lists.h"
 
 /**
- *
- *  * print_listint - Entry point.
- *
- *   * @h:value.
- *
- *    * Return: Always 0 (
+ * free_listint2 - Entry point.
+ * @head:pointer value.
+ * Return: Always 0 (Success)
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *node;
+
+	if (head)
+	{
+		if (*head)
+		{
+			while (*head != NULL)
+			{
+				node = *head;
+				*head = (*head)->next;
+				free(node);
+			}
+		}
+		*head = NULL;
+	}
+}
