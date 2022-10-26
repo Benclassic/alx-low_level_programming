6cb7791eb680bb1b4dd19830d83033f4ea4c7e5c
Benneth <110991703+Benclassic@users.noreply.github.com>
2022-10-26 09:29:37 +0100

    Update 6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,11 +1,22 @@
-nclude "lists.h"
-
-
+#include "lists.h"
 
 /**
- *
- *  * print_listint - Entry point.
- *
- *   * @h:value.
- *
- *    * Return: Always 0 (
+ * pop_listint - Entry point.
+ * @head:pointer value.
+ * Return: Always 0 (Success)
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *node;
+	int n;
+
+	if (*head)
+	{
+		n = (*head)->n;
+		node = (*head)->next;
+		free(*head);
+		*head = node;
+		return (n);
+	}
+	return (0);
+}
