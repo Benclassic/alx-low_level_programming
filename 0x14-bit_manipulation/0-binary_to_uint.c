7946225a1e34be9c1fef8623a2e7b4040221092c
Benneth <enwerembenneth@gmail.com>
2022-10-27 01:40:09 -0700

    Task0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,13 @@
+nclude "main.h"
+
+
+
+/**
+ *
+ *  * binary_to_uint - function that converts a binary number to an unsigned int.
+ *
+ *   * @b: pointer to the string of 0's and 1's
+ *
+ *    * Return: an unsigned int number
+ *
+ *     */
