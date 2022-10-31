d8958b1f8a05986ddb4899278a866a342b78a6b2
Benneth <110991703+Benclassic@users.noreply.github.com>
2022-10-31 12:22:38 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,6 +15,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
+
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
@@ -26,12 +27,12 @@ int create_file(const char *filename, char *text_content)
 	for (nletters = 0; text_content[nletters]; nletters++)
 		;
 
-		rwr = write(fd, text_content, nletters);
-		
-		if (rwr == -1)
-			return (-1);
+	rwr = write(fd, text_content, nletters);
 
-		close(fd);
+	if (rwr == -1)
+		return (-1);
 
-		return (1);
+	close(fd);
+
+	return (1);
 }
